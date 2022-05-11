e834a9300eab9ac9bcba94a1140ee4ffc1d07ae9
jaredsanderson <jaredsa@byui.edu>
2022-05-11 09:00:34 -0600

    Swagger Documentation Added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
+
 const routes = express.Router();
 // const name = require('../controllers/');
+
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 routes.use('/contacts', require('./contacts'));
 
